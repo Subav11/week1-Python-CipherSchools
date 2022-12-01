2186988b280ff939f81cf251640de46de253d728
DESKTOP-LVBNDTT\asus <subhamyadav7528@gmail.com>
2022-12-01 23:20:14 +0530

    week1 files uploaded

---
+++ Chapter3-the__in-keyword-CipherSchools.py
@@ -0,0 +1,7 @@
+name = "Subham"
+#  in keyword
+#  if with in 
+if  "h" in "Subham":
+    print("h is present in the name")
+else:
+    print("not present")
---
+++ Chapter3-while_sum -CipherSchools.py
@@ -0,0 +1,8 @@
+# Sum of Numbers Program using while loop in Python
+
+total = 0
+i = 1
+while i <= 10:
+    total += i
+    i += 1
+print(total)
